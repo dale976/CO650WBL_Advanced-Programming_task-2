562e6c65dc269faca2c0e2fcb38f1fef7d3fbf36
Alan Dale <alan.dale@sky.uk>
2024-08-24 21:46:58 +0100

    commit cmake files and client code from exercise

---
+++ build/CMakeFiles/CLIENT_SOCKET.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CLIENT_SOCKET"
+  "CLIENT_SOCKET.pdb"
+  "CMakeFiles/CLIENT_SOCKET.dir/src/main.cpp.o"
+  "CMakeFiles/CLIENT_SOCKET.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLIENT_SOCKET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
