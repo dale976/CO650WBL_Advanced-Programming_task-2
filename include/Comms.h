6aaf61c8ec7721eeea54caf83ff3464630c0aad9
Alan Dale <alan.dale@sky.uk>
2024-08-31 17:10:07 +0100

    split out comms class into cpp file

--- include/Comms.h
+++ include/Comms.h
@@ -2,9 +2,7 @@
 #define SHARED_H
 
 #include <iostream>
-#include <arpa/inet.h>
 #include <netdb.h>
-#include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -12,45 +10,22 @@ using namespace std;
 
 class Comms {
     public:
-        Comms(): base_socket(-1){
-            cout << "Comms constructor" << endl;
-        }; // any other constructor code?
-        virtual ~Comms(){
-            cout << "Comms deconstructor" << endl;
-            closeConnetion();
-        }; // destructor to be overriden in subclass
-        bool initialise() {
-            cout << "Comms socket initialise" << endl;
-            base_socket = socket(AF_INET, SOCK_STREAM, 0);
-            if (base_socket < 0) {
-                handleError("Error creating socket");
-                return false;
-            }
-            return true;
-        };
-        void closeConnetion(){
-            cout << "Comms close connection" << endl;
-            if (base_socket != 0) {
-                close(base_socket);
-                cout << "Connection Closed" << endl;
-            }
-        };
-    protected: // Make protected so only this class and any subclasses can access the following members
+        Comms();
+        virtual ~Comms();
+        bool initialise();
+        void closeConnetion();
+    // Protected members; Only this class and any subclasses can access the following members
+    protected: 
         const int PORT = 5400;
         const string IP_ADDRESS = "127.0.0.1";
         int base_socket;
         sockaddr_in service;
+        // pure virtual methods, 0 value to indicate to the compiler that this is an empty method
+        virtual void connect() = 0;
+        virtual void send(const string &msg) = 0;
+        virtual void sendAndReceive() = 0;
 
-        virtual void connect(){}; // override in subclass
-        virtual void send(const string &msg){}; // override in subclass
-        virtual void sendAndReceive() {}; // override in subclass
-
-        void handleError(const string &msg){
-            cerr << msg << endl;
-            exit(1);
-        };
-
-        
+        void handleError(const string &msg);
 };
 
 #endif // SHARED_H
--- src/Comms.cpp
+++ src/Comms.cpp
@@ -1,6 +1,40 @@
 #include "Comms.h"
 #include <iostream>
 
+using namespace std;
+
 void sharedFunction() {
     std::cout << "This is a shared function." << std::endl;
 }
+
+Comms::Comms(): base_socket(-1){
+    cout << "Comms constructor" << endl;
+};
+
+Comms::~Comms(){
+    cout << "Comms deconstructor" << endl;
+    closeConnetion();
+};
+
+bool Comms::initialise() {
+    cout << "Comms socket initialise" << endl;
+    base_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (base_socket < 0) {
+        handleError("Error creating socket");
+        return false;
+    }
+    return true;
+};
+
+void Comms::closeConnetion(){
+    cout << "Comms close connection" << endl;
+    if (base_socket != 0) {
+        close(base_socket);
+        cout << "Connection Closed" << endl;
+    }
+};
+
+void Comms::handleError(const string &msg){
+    cerr << msg << endl;
+    exit(1);
+};
